b7cf8838b6f9d608395bf4a619328c23d06a3164
Luka Mirosevic <luka@goonbee.com>
2013-01-28 14:49:59 +0100

    Initial Commit

---
+++ GBVersionTracking/GBVersionTracking.h
@@ -0,0 +1,13 @@
+//
+//  GBVersionTracking.h
+//  GBVersionTracking
+//
+//  Created by Luka Mirosevic on 28/01/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBVersionTracking : NSObject
+
+@end
---
+++ GBVersionTracking/GBVersionTracking.m
@@ -0,0 +1,13 @@
+//
+//  GBVersionTracking.m
+//  GBVersionTracking
+//
+//  Created by Luka Mirosevic on 28/01/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBVersionTracking.h"
+
+@implementation GBVersionTracking
+
+@end
