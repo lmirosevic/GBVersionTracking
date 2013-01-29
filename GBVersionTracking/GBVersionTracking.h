199b325bffda2c9f299db79328277ccf404825d6
Luka Mirosevic <luka@goonbee.com>
2013-01-29 14:14:49 +0100

    code done

--- GBVersionTracking/GBVersionTracking.h
+++ GBVersionTracking/GBVersionTracking.h
@@ -28,10 +28,24 @@
 
 /* Demo
  
+ //Call this in application:didFinishLaunching:withOptions:
  [GBVersionTracking track];
  
- l(@"current build: %@\nprevios build: %@\ncurrent version: %@\nprevious version: %@\nisFirstLaunchEver: %@\nfirstlaunchforbuild:%@\nfirstlaunchforversion:%@\n", [GBVersionTracking currentBuild], [GBVersionTracking previousBuild], [GBVersionTracking currentVersion], [GBVersionTracking previousVersion], _b([GBVersionTracking isFirstLaunchEver]), _b([GBVersionTracking isFirstLaunchForBuild]), _b([GBVersionTracking isFirstLaunchForVersion]));
- l(@"b his: %@", [GBVersionTracking buildHistory]);
- l(@"v his %@\n\n", [GBVersionTracking versionHistory]);
+ //Call this anywhere
+ l(@"First launch ever?: %@", _b([GBVersionTracking isFirstLaunchEver]));
+ l(@"First launch for this version?: %@", _b([GBVersionTracking isFirstLaunchForVersion]));
+ l(@"First launch for this build?: %@", _b([GBVersionTracking isFirstLaunchForBuild]));
+ 
+ l(@"Current version: %@", [GBVersionTracking currentVersion]);
+ l(@"Previous version: %@", [GBVersionTracking previousVersion]);
+ l(@"Version history: %@", [GBVersionTracking versionHistory]);
+ 
+ l(@"Current build: %@", [GBVersionTracking currentBuild]);
+ l(@"Previous build: %@", [GBVersionTracking previousBuild]);
+ l(@"Build history: %@", [GBVersionTracking buildHistory]);
+ 
+ 
+ Required Libraries (make sure header search path is set up correctly in parent project, there is a linker flag -ObjC):
+ * GBToolbox
  
  */
